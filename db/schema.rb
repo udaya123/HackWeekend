f713c8427d13c4dd0e7033092bdb57619f7b2d3f
Keenan Johnson <keenan.johnson1@outlook.com>
2016-07-30 16:39:12 -0500

    Add email migrtion to schema and email to add contact form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730190904) do
+ActiveRecord::Schema.define(version: 20160730213626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160730190904) do
     t.integer  "company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
   add_index "contacts", ["company_id"], name: "index_contacts_on_company_id", using: :btree
