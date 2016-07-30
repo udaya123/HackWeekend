a6239cbefadfe2034edb00b374ef3bce28130f48
udayaratna <udaya.ratna@gmail.com>
2016-07-30 11:41:57 -0700

    added changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :companies
+  
   devise_for :users
   root to: 'pages#index'
-  resoucres :contacts
+  resources :contacts
   resources :companies
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
