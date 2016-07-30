f6a63ce152d773e25a11097115df68d93d11d0a6
Tony Goldsmith <tony.h.goldsmith@gmail.com>
2016-07-30 14:31:10 -0500

    added paperclip to upload company image

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,10 +1,13 @@
 class Company < ActiveRecord::Base
+  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
+
   milestone = {
   	#Point value is out of 1000
   	#We will need to add validation to make sure number can't be negative or exceed 1000
   	"emailed" => 50,
-  	"contacted" => 100, 
-  	"applied" => 50, 
+  	"contacted" => 100,
+  	"applied" => 50,
   	"asked_for_coffee" => 100,
   	"got_coffee" => 150,
   	"viewed_space" => 50,
