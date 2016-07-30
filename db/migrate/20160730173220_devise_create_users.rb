82171a7b5c94a93561b06ea9a62886a3781a147b
Stefan <santonrod@gmail.com>
2016-07-30 12:49:54 -0500

    migrate devise table

--- db/migrate/20160730173220_devise_create_users.rb
+++ db/migrate/20160730173220_devise_create_users.rb
@@ -2,9 +2,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.boolean :admin, null: false, default: false 
+      t.integer :points_total, null: false, default: 0
+      t.string  :cohort, null: false, default: ""
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -24,12 +24,15 @@ ActiveRecord::Schema.define(version: 20160730173220) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.boolean  "admin",                  default: false, null: false
+    t.integer  "points_total",           default: 0,     null: false
+    t.string   "cohort",                 default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
